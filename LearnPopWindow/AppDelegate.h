c884246a91f7622ee668c7b76fefbd8ed4406449
confiwang <confiwang@tencent.com>
2017-07-14 21:15:11 +0800

    Initial Commit

---
+++ LearnPopWindow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LearnPopWindow
+//
+//  Created by confiwang on 2017/7/14.
+//  Copyright © 2017年 TencentJobs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ LearnPopWindow/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  LearnPopWindow
+//
+//  Created by confiwang on 2017/7/14.
+//  Copyright © 2017年 TencentJobs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
